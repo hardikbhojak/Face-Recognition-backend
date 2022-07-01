982335c9ca5547c1282c29221ee74c3e1fa6e42b
HardikBhojak <103426537+HardikBhojak@users.noreply.github.com>
2022-07-01 20:06:00 +0530

    updating database 1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const profile = require("./controllers/profile");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
     // port: "5432",
     // user: "postgres",
